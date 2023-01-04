4c8ae0e1d47551186344e10089e53880ee9f0ead
jooapas <jooa.akonpelto@gmail.com>
2023-01-05 01:38:45 +0200

    stuff

--- Scripts/enemy.cs
+++ Scripts/enemy.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
-
+    [SerializeField] int ScorePerHit = 1;
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int Hitpoints = 2;
+
+    Scoreboard score;
+    void Start()
+    {
+       score = FindObjectOfType<Scoreboard>(); //findOBjectOfType very recource heavy
+
+    }
     void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (Hitpoints <= 0){
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        Hitpoints --;
+        score.IncreaseScore(ScorePerHit);
+    }
+    void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
-        Destroy(gameObject);       
+        Destroy(gameObject);
     }
 }
